9989f923dc56a7ccc60b69bf1e27da0532faa5ec
oliver zulett <oliverzulett@gmail.com>
2024-01-27 07:39:20 -0400

    crear un observable con parametros

--- main.js
+++ main.js
@@ -1,26 +1,9 @@
 import { Observable } from 'rxjs';
 
-
-return  function observer(subscriber) {
-  subscriber.next(1);
-  subscriber.next(2);
-  subscriber.next('3');
-  setTimeout(() => {
-    subscriber.next(4);
-  }, 500);
-  subscriber.next(5);
-
-  let intervalValue = 0;
-  setInterval(() => {
-    subscriber.next(`Interval value: ${intervalValue++}`);
-  }, 200);
-
-  setTimeout(() => {
-    // subscriber.complete()
-    subscriber.error(new Error('error from observable'))
-  }, 2000);
+const observerWithParams = (params) => function observer(subscriber) {
+  subscriber.next(params)
 }
 
-const observable = new Observable();
+const observable = new Observable(observerWithParams('Hello World!'));
 
 observable.subscribe(console.log);
