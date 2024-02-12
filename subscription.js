77476b50bfed4bae13445c44e60eeafe113151d5
oliver zulett <oliverzulett@gmail.com>
2024-02-12 11:15:12 -0400

    clean branch for angular

--- subscription.js
+++
@@ -1,9 +0,0 @@
-import { interval } from 'rxjs';
-
-const observable = interval(100);
-
-const subscription = observable.subscribe(console.log);
-
-setTimeout(() => {
-  subscription.unsubscribe();
-}, 1000);
