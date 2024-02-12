6d65426e825a72f32bc68f460de6d8cb3d8d6673
oliver zulett <oliverzulett@gmail.com>
2024-02-12 11:43:36 -0400

    modificar la ui con observables

--- observables-con-angular/src/app/app.component.ts
+++ observables-con-angular/src/app/app.component.ts
@@ -4,22 +4,35 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  valueFromObservable: number;
+
+  private observable: Observable<number> = new Observable((observer) => {
+    // observer.next(1),
+    // observer.next(2),
+    // observer.next(3),
+    // observer.complete()
+    let intervalValue = 1;
+
+    setInterval(() => {
+      observer.next(intervalValue++);
+    }, 500);
+
+    setTimeout(() => {
+      observer.complete();
+    }, 5000);
+  });
 
   constructor() {
-    const observable = new Observable(observer => {
-      observer.next(1),
-      observer.next(2),
-      observer.next(3),
-      observer.complete()
+    this.valueFromObservable = 0;
+
+    this.observable.subscribe({
+      next: (value: number) => {
+        this.valueFromObservable = value;
+      },
+      complete: () => console.log('complete'),
     });
-
-    observable.subscribe({
-      next: (console.log),
-      complete: () => (console.log('complete'))
-    })
   }
-
 }
