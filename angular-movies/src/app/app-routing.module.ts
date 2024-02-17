cbba997797a87940c3f13142e7ac6a75a466fb7e
oliver zulett <oliverzulett@gmail.com>
2024-02-17 07:02:10 -0400

    rutas

--- angular-movies/src/app/app-routing.module.ts
+++ angular-movies/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieComponent } from './pages/movie/movie.component';
+import { MovieListComponent } from './pages/movie-list/movie-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'movies',
+    component: MovieListComponent,
+  },
+  {
+    path: 'movie/:id',
+    component: MovieComponent,
+  },
+  {
+    path: '**',
+    component: MovieListComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
