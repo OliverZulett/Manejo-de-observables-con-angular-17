bc57b8d9e71d1e645661aeeac02dd751347a2b03
oliver zulett <oliverzulett@gmail.com>
2024-02-17 14:30:59 -0400

    pagina de pelicula

--- angular-movies/src/app/pages/pages.module.ts
+++ angular-movies/src/app/pages/pages.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { MovieComponent } from './movie/movie.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [MovieListComponent, MovieComponent],
   imports: [
-    CommonModule
+    CommonModule, RouterModule
   ],
   exports: [MovieListComponent, MovieComponent],
 })
