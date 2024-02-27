62a94d9cc664d892a8d6e73047851b119e7ef920
Oliver Zulett <oliverz@hubx.com>
2024-02-26 20:46:38 -0400

    funcionalidad del formulario

--- angular-movies/src/app/shared/movie-form/movie-form.component.ts
+++ angular-movies/src/app/shared/movie-form/movie-form.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-movie-form',
   templateUrl: './movie-form.component.html',
-  styleUrl: './movie-form.component.scss'
+  styleUrl: './movie-form.component.scss',
 })
-export class MovieFormComponent {
+export class MovieFormComponent implements OnInit {
+  movieForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.movieForm = this.formBuilder.group({
+      title: '',
+      overview: '',
+      image: '',
+      'release-date': '',
+      budget: 0,
+      revenue: 0,
+      popularity: 0,
+      homepage: '',
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.movieForm.value);
+  }
 }
